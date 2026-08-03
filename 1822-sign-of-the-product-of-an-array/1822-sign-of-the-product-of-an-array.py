47dce57a36ccf7d5e5c61618ba2433a4eb85a40f
Sravya <sadhusravya123@gmail.com>
2026-08-03 21:06:55 +0530

    Time: 0 ms (100%), Space: 12.5 MB (17.01%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def arraySign(self, nums):
+        sign = 1
+        for num in nums:
+            if num == 0:
+                return 0
+            elif num < 0:
+                sign *= -1
+
+        return sign
